636fa272995e8268ef53f8443d7fa1e9b321566b
yyx <yyx1025988443@gmail.com>
2018-03-18 19:37:40 +0800

    multi-config

--- src/main/java/cn/yangyuanxin/controller/IndexController.java
+++ src/main/java/cn/yangyuanxin/controller/IndexController.java
@@ -64,7 +64,7 @@ public class IndexController {
 
     @RequestMapping("/index")
     public String index(Model model) {
-        return "/index";
+        return "index";
     }
 
     @RequestMapping("/contact")
